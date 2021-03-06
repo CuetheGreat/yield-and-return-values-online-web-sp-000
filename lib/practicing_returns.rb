c47c5ab17d9c323dc6c7baa00b44b744600353b0
Clayon Stewart <c.stewart89@gmail.com>
2021-03-06 19:15:02 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,11 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry
     yield(array[i])
     i += 1
   end
 end
 
-binding.pryexit
+
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
